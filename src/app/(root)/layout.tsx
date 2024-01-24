14e080d891a0cfb0f52647604d12530f0bac8fe8
Olavo Mars <olavoamneves@gmail.com>
2024-01-24 13:22:34 -0400

    first commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import '../globals.css';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Loomia - design & development',
+  description:
+    "Your Startup's Design & Development Ally. Upgrade Your Process.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className='w-full h-screen flex justify-center'>
+      <div className='w-full max-w-screen-xl flex justify-start'>
+        <div>
+          <h1 className='text-[16rem] font-extrabold'>Loomify</h1>
+          <h2>Your Startup&apos;s Design & Development Ally</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
