e0266fc114fa26ec63b8810c7e2773820980e5ac
Olavo Mars <olavoamneves@gmail.com>
2024-01-26 12:40:56 -0400

    possibly null X and Y fix

--- src/components/shared/Cursor.tsx
+++ src/components/shared/Cursor.tsx
@@ -7,11 +7,13 @@ export default function Cursor() {
 
   return (
     <div className='max-lg:hidden absolute w-full h-full pointer-events-none '>
-      <motion.div
-        className={`bg-primary rounded-full fixed w-6 h-6`}
-        animate={{ opacity: 1, scale: 1, x: x - size / 2, y: y - size / 2 }}
-        transition={{ type: 'tween', ease: 'backOut', duration: 0.5 }}
-      ></motion.div>
+      {x && y && (
+        <motion.div
+          className={`bg-primary rounded-full fixed w-6 h-6`}
+          animate={{ opacity: 1, scale: 1, x: x - size / 2, y: y - size / 2 }}
+          transition={{ type: 'tween', ease: 'backOut', duration: 0.5 }}
+        ></motion.div>
+      )}
     </div>
   );
 }
